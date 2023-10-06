93d2f02afa64da96e2305ea2c548a9a7603b66e7
arbelhadari <arbelhadari@gmail.com>
2023-10-06 13:47:08 +0300

    change labels dict structure to contain the full path to FASTA file

--- utils/FASTA_parser.py
+++ utils/FASTA_parser.py
@@ -1,19 +1,22 @@
 from Bio import SeqIO
 import pandas as pd
+import os
 
 
 class FastaParser:
 
     @staticmethod
-    def get_train_labels_dict(labels_path, remove_prefix=True):
-        """Transform training labels file to dict.
+    def get_train_labels_dict(training_path: str):
+        """Transform training labels file to dict (label includes path to actual FASTA file).
 
-        :param labels_path: path to training labels (.txt file).
-        :param remove_prefix: if True, removes prefix from paths.
+        :param training_path: path to training data and labels
         :return: dict of labels and their matching FASTA files.
         """
+        labels_path = os.path.dirname(training_path)
+        full_path = os.path.join(training_path, "centroids")
+
         df = pd.read_csv(labels_path, sep='\t', header=None, index_col=0)
-        df[1] = df[1].str.replace('centroids/', '') if remove_prefix else df[1]
+        df[1] = df[1].str.replace('centroids', full_path)
         return df.to_dict()[1]
 
     @staticmethod
